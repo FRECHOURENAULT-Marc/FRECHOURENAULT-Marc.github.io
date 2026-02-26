1345ad544c81da564e53759b8792a0b17e1458ea
FRECHOURENAULT-Marc <mfrechourenault@gaming.tech>
2026-02-26 09:03:55 +0100

    add cv fr

--- js/utils.js
+++ js/utils.js
@@ -61,7 +61,16 @@ document.addEventListener('DOMContentLoaded', () =>
   })                                                    
 });
 
-
+function Download(file, name)
+{
+  const link = document.createElement("a");
+  link.href = file;
+  link.download = name;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  link.remove();
+}
 
 
 
